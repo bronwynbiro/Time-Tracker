f1c1e5dcaf88c91a9a404b94656c940c22a773be
bronwynbiro <bronwyn.biro@gmail.com>
2016-06-21 18:26:47 -0600

    "added base case for beginning of percentages of each activity per month function."

--- TimeTracker/AppDelegate.swift
+++ TimeTracker/AppDelegate.swift
@@ -4,14 +4,15 @@
 // V1
 
 import UIKit
+import CoreData
 
 /**
-    Application appDelegate that is responsible for loading the application.
-*/
+ Application appDelegate that is responsible for loading the application.
+ */
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     
     var window: UIWindow?
     class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,38 +20,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         var window: UIWindow?
         func application(application: UIApplication,
                          didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        return true
+            return true
         }
-
-    /**
-        Save app's state to load it back when the user comes back.
-
-        - parameter application: application
-    */
-    func applicationDidEnterBackground(application: UIApplication) {
-        NSNotificationCenter.defaultCenter().postNotificationName("AppDidEnterBackground", object: nil)
-        //MARK: save coredata too
-         CoreDataHandler.sharedInstance.saveContext()
+        
+        /**
+         Save app's state to load it back when the user comes back.
+         
+         - parameter application: application
+         */
+        func applicationDidEnterBackground(application: UIApplication) {
+            NSNotificationCenter.defaultCenter().postNotificationName("AppDidEnterBackground", object: nil)
+            //MARK: save coredata too
+            CoreDataHandler.sharedInstance.saveContext()
+        }
+        
+        /**
+         Loads the passed seconds if the timer was active when the app was closed.
+         
+         - parameter application: application
+         */
+        func applicationDidBecomeActive(application: UIApplication) {
+            NSNotificationCenter.defaultCenter().postNotificationName("AppBecameActive", object: nil)
+        }
+        
+        /**
+         Save core data context if the app is closed.
+         
+         - parameter application: application
+         */
+        func applicationWillTerminate(application: UIApplication) {
+            CoreDataHandler.sharedInstance.saveContext()
+        }
+        
     }
-
-    /**
-        Loads the passed seconds if the timer was active when the app was closed.
-
-        - parameter application: application
-    */
-    func applicationDidBecomeActive(application: UIApplication) {
-        NSNotificationCenter.defaultCenter().postNotificationName("AppBecameActive", object: nil)
-    }
-
-    /**
-        Save core data context if the app is closed.
-
-        - parameter application: application
-    */
-    func applicationWillTerminate(application: UIApplication) {
-        CoreDataHandler.sharedInstance.saveContext()
-    }
-
-}
-
+    
 }
