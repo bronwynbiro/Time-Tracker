12e6ea86d396c8d0c0dc815d34641d390539206c
bronwynbiro <bronwyn.biro@gmail.com>
2016-07-10 17:26:42 -0600

    now shows percentage of activity

--- TimeTracker/viewcontrollers/ProgressViewController.swift
+++ TimeTracker/viewcontrollers/ProgressViewController.swift
@@ -14,19 +14,21 @@ import CoreData
 
 //class ProgressViewController: UIViewController, UITableViewDelegate, NSFetchedResultsControllerDelegate {
 class ProgressViewController: UIViewController {
+    @IBOutlet weak var testLabel: UITextField!
     @IBOutlet weak var dailyButton: UIButton!
     @IBOutlet weak var weeklyButton: UIButton!
     @IBOutlet weak var monthlyButton: UIButton!
-    @IBOutlet weak var testLabel: UITextField!
     
+    @IBOutlet weak var liftText: UITextField!
+    @IBOutlet weak var workText: UITextField!
     @IBAction func calculateTodaysActivities(sender: UIButton) {
-    var todaysActivitiesArray = CoreDataHandler.sharedInstance.fetchCoreDataForTodayActivities()
-    var sumOfToday = 0
-    if todaysActivitiesArray.count > 0 {
-        for history in todaysActivitiesArray {
-            sumOfToday += (history.duration?.integerValue)!
+        var todaysActivitiesArray = CoreDataHandler.sharedInstance.fetchCoreDataForTodayActivities()
+        var sumOfToday = 0
+        if todaysActivitiesArray.count > 0 {
+            for history in todaysActivitiesArray {
+                sumOfToday += (history.duration?.integerValue)!
+            }
         }
-    }
         testLabel.text = "\(NSString.createDurationStringFromDuration(Double(sumOfToday)))"
     }
     
@@ -43,17 +45,44 @@ class ProgressViewController: UIViewController {
     
     @IBAction func calculateMonthlyActivities(sender: UIButton) {
         var monthActivitiesArray = CoreDataHandler.sharedInstance.fetchCoreDataForMonthActivities()
-        var sumOfMonth = 0
+        var sumOfMonth: Double = 0
         if monthActivitiesArray.count > 0 {
             for history in monthActivitiesArray {
-                sumOfMonth += (history.duration?.integerValue)!
+                sumOfMonth += (history.duration?.doubleValue)!
             }
+            
         }
         testLabel.text = "\(NSString.createDurationStringFromDuration(Double(sumOfMonth)))"
+        var namesArray = [String]()
+        for histname in monthActivitiesArray{
+            namesArray.insert(histname.name!, atIndex: 0)
+        }
+        var percentArray = [String]()
+        
+        let unique = Array(Set(namesArray))
+        
+        var sum: Double = 0
+        var percentage: Double = 0
+        
+        for i in unique.indices{
+            var activArr = CoreDataHandler.sharedInstance.filterResults(unique[i])
+            var uniqueActivArr = Array(Set(activArr))
+            sum = 0
+            for myObj in uniqueActivArr {
+                if myObj.name == "lift" {
+                    sum += (myObj.duration?.doubleValue)!
+                    percentage = (sum / Double(sumOfMonth))*100
+                    print("\(unique[i]): percent \(percentage)")
+                    liftText.text = "Percentage of \(unique[i]): \(round(percentage))%"
+                }
+                else {
+                    sum += (myObj.duration?.doubleValue)!
+                    percentage = (sum / Double(sumOfMonth))*100
+                    print("\(unique[i]): percent \(percentage)")
+                    workText.text = "Percentage of \(unique[i]): \(round(percentage))%"
+                    
+                }
+            }
+        }
     }
-
-    
-    
-    
-    
 }
