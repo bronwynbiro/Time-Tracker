61d0a6f63904b3aa6bcd729a904744892691ced1
bronwynbiro <bronwyn.biro@gmail.com>
2016-07-16 21:39:58 -0600

    fixed compilation errors for simulator but not for testing on iphone yet

--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
-platform :ios, ‘8.0’
-use_frameworks!
+platform :ios, ‘9.3’
+#use_frameworks!
  
 target ’TimeTracker’ do
     pod 'Charts'
---
+++ Pods/Pods/Target Support Files/Pods-TimeTracker/Pods-TimeTracker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TimeTracker : NSObject
+@end
+@implementation PodsDummy_Pods_TimeTracker
+@end
